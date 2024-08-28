87f52f688a4b6b6926ff9541675a57d0574d681c
ElshadRyan <117740569+ElshadRyan@users.noreply.github.com>
2024-08-29 01:01:52 +0700

    Update ExecuteLogic.cs

--- Assets/Script/ExecuteLogic.cs
+++ Assets/Script/ExecuteLogic.cs
@@ -2,10 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/* PERHATIAN!!!
+ * Kalo mau akses logic di skrip ini
+ * Public class nya extend ke class ini
+ * Jangan ke MonoBehaviour
+ */
 public class ExecuteLogic : MonoBehaviour
 {
 
     //logic 'Shoot'
+    //setelah di extend, klean bisa make function ini tanpa perlu declare
     public void Shoot()
     {        
         SpawnBullet bullet = GetComponentInChildren<SpawnBullet>();
